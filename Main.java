e9eb8730d761d4eee5e9bfed03d976ec23a3ed29
vaibavv360-create <vaibavv360@gmail.com>
2026-04-01 09:04:11 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,15 @@
- 
-import java.util.Scanner; 
- 
+// File: Main.java 
 public class Main { 
-
     public static void main(String[] args) { 
+        UserRegistration app = new UserRegistration(); 
  
-        Scanner sc = new Scanner(System.in); 
+        // Scenario 1: Valid 
+        app.processRegistration("JavaExpert", 30); 
  
-        System.out.print("Enter first number: "); 
-        int num1 = sc.nextInt(); 
+        // Scenario 2: Invalid Age 
+        app.processRegistration("JuniorDev", -1); 
  
-        System.out.print("Enter second number: "); 
-        int num2 = sc.nextInt(); 
- 
-        // Call static methods directly using class name 
-        ArithOp.add(num1, num2); 
-        ArithOp.sub(num1, num2); 
-        ArithOp.mul(num1, num2); 
-        ArithOp.div(num1, num2); 
- 
-        sc.close(); 
+        // Scenario 3: Invalid Username 
+        app.processRegistration("", 20); 
     } 
-} 
+}
